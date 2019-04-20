5ca7bc589ddc134b96919f30c4db0bc3ad497b10
Jesse Brewer <jbrewerwriter@gmail.com>
2019-04-19 20:53:31 -0400

    Added searchbar, dynamic cards

--- dev/weather/weather-list.component.ts
+++ dev/weather/weather-list.component.ts
@@ -1,14 +1,23 @@
-import {Component} from "angular2/core";
+import {Component, OnInit} from "angular2/core";
 import {WeatherItemComponent} from "./weather-item.component";
+import { WEATHER_ITEMS } from './weather.data';
+import { WeatherService } from "./weather.service";
 @Component({
     selector: 'weather-list',
     template: `
         <section class="weather-list">
-            <weather-item></weather-item>
+            <weather-item *ngFor="#weatherItem of weatherItems" [item]="weatherItem"></weather-item>
         </section>
     `,
-    directives: [WeatherItemComponent]
+    directives: [WeatherItemComponent],
+    providers: [WeatherService]
 })
-export class WeatherListComponent {
+export class WeatherListComponent implements OnInit {
+    weatherItems: WeatherItem[];
 
+    constructor(private _weatherService: WeatherService) {}
+
+    ngOnInit():any {
+        this.weatherItems = this._weatherService.getWeatherItems();
+    }
 }
---
+++ dev/weather/weather.data.ts
@@ -0,0 +1,6 @@
+import { WeatherItem } from './weather-item';
+export const WEATHER_ITEMS: WeatherItem[] = [
+    new WeatherItem('Granger', 'SUNNY', 24),
+    new WeatherItem('Mishawaka', 'CLOUDY', 30),
+    new WeatherItem('Chicago', 'CLOUDY', 32)
+];
